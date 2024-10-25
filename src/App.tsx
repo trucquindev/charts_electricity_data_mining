f1f24375c96ef380520d06e80d609a4b6d8336b2
quynhdeptrai306 <nguyentrucquynh3006@gmail.com>
2024-10-25 21:09:56 +0700

    init project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from './components/ui/button';
+function App() {
+  return (
+    <div className="text-red-400">
+      <Button>Helo anh em</Button>
+    </div>
+  );
+}
+
+export default App;
